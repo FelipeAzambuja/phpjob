421cc789f91f10c8a9e2e575f108ba0ab8b066b2
Felipe Nunes Azambuja <felipenunesazambuja@gmail.com>
2017-12-12 11:52:16 -0200

    Apenas atualizando

--- job.php
+++ job.php
@@ -11,6 +11,7 @@ use Anekdotes\File\File;
 use FtpClient\FtpClient;
 use GuzzleHttp\Client;
 use League\CLImate\CLImate;
+
 include $argv[1];
 eval($argv[2] . "(" . "'" . implode("','", array_splice($argv, 3)) . "'" . ");");
 
